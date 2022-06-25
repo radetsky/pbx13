a612c6c6567b63c5efd4d5f5de013a07d105b43a
Oleksii Radetskyi <alexey.radetsky@gmail.com>
2022-06-25 23:18:23 +0200

    static settings

--- pearlpbx13/pearlpbx13/urls.py
+++ pearlpbx13/pearlpbx13/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.urls import include, path
 from django.urls import path
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('pbxsetup/', include('pbxsetup.urls')),
     path('agentconf/', include('agentconf.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# print(urlpatterns)
