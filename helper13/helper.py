42d6933fecb25d31a8df209fd521268c2e6cef3d
Oleksii Radetskyi <alexey.radetsky@gmail.com>
2022-07-22 01:19:27 +0300

    add settings module to the helper

--- helper13/helper.py
+++ helper13/helper.py
@@ -3,6 +3,7 @@ Wait for POST from allowed (TODO) IP and allowed (TODO) Themis key
 '''
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import settings
 
 
 def my_handler(path):
@@ -10,7 +11,19 @@ def my_handler(path):
 
 
 class MyHandler(BaseHTTPRequestHandler):
+    def check_allowed_ip(self):
+        '''
+        check if request is from allowed IP
+        '''
+        if self.client_address[0] not in settings.ALLOWED_HOST:
+            self.send_error(403)
+            return False
+        return True
+
     def do_GET(self):
+        '''
+        return 404
+        '''
         self.send_response(404)
         self.send_header("Content-type", "text/html")
         self.end_headers()
@@ -18,6 +31,9 @@ class MyHandler(BaseHTTPRequestHandler):
         return
 
     def do_POST(self):
+        if not self.check_allowed_ip():
+            return
+
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
@@ -25,5 +41,5 @@ class MyHandler(BaseHTTPRequestHandler):
         return
 
 
-httpd = HTTPServer(("", 8080), MyHandler)
+httpd = HTTPServer((settings.BIND, settings.PORT), MyHandler)
 httpd.serve_forever()
